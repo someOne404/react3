2e50f7028995a2cd36de9a062496ea7e1be1fb25
someOne404 <jiayilu96@gmail.com>
2020-01-26 15:18:44 -0500

    Assignment 1 updated

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,18 @@ import { Card } from 'react-native-elements';
 import { ListItem } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 
+function RenderHistory() {
+    return (
+        <Card title="Our History">
+            <Text style={{margin: 10}}>
+                Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
+
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+            </Text>
+        </Card> 
+    );
+}
+
 class About extends Component {
 
     constructor(props) {
@@ -35,13 +47,7 @@ class About extends Component {
 
         return (
             <ScrollView>
-                <Card title="Our History">
-                    <Text style={{margin: 10}}>
-                        Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
-
-                        The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
-                    </Text>
-                </Card> 
+                <RenderHistory />
                 <Card title="Coporate Leadership">
                     <FlatList
                         data={this.state.leaders}
